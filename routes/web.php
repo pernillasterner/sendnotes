e3a5130382fc85819995fa0ba365d0f2ce0d8684
pernillasterner <psterner@hotmail.com>
2025-01-19 13:28:42 +0100

    Add route for note page

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,8 @@ Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
 
+Route::view('notes', 'notes')
+    ->middleware(['auth'])
+    ->name('notes');
+
 require __DIR__.'/auth.php';
